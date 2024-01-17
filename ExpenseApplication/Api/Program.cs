fe2ce6629c186bee3356eea58c10d0cfab042d97
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-18 01:00:43 +0300

    ExpenseApplication/Api/Program.cs

--- ExpenseApplication/Api/Program.cs
+++ ExpenseApplication/Api/Program.cs
@@ -1,44 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+namespace Api;
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public static void Main(string[] args)
+    {
+        var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        // Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(config).CreateLogger();
+        // Log.Information("App server is starting.");
+        
+        Host.CreateDefaultBuilder(args)
+            // .UseSerilog()
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            }).Build().Run();
+    }
 }
