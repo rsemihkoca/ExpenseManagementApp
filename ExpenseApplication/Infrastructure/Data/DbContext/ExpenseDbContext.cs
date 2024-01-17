ad9713133c86b773e3e7db3e441850e4b1da6b0b
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-17 23:38:20 +0300

    ExpenseApplication/Infrastructure/Data/DbContext/ExpenseDbContext.cs

--- ExpenseApplication/Infrastructure/Data/DbContext/ExpenseDbContext.cs
+++ ExpenseApplication/Infrastructure/Data/DbContext/ExpenseDbContext.cs
@@ -1,6 +1,26 @@
+using Business.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace Infrastructure.Data.DbContext;
 
-public class ExpenseDbContext
+public class ExpenseDbContext : Microsoft.EntityFrameworkCore.DbContext
 {
+
+    public ExpenseDbContext(DbContextOptions<ExpenseDbContext> options): base(options)
+    {
     
+    }   
+    public DbSet<User> Users { get; set; }
+    public DbSet<Expense> Expenses { get; set; }
+    public DbSet<ExpenseCategory> ExpenseCategories { get; set; }
+    public DbSet<PaymentInstruction> PaymentInstructions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserConfiguration());
+        modelBuilder.ApplyConfiguration(new ExpenseRequestConfiguration());
+        modelBuilder.ApplyConfiguration(new ExpenseCategoryConfiguration());
+        modelBuilder.ApplyConfiguration(new PaymentInstructionConfiguration());
+        base.OnModelCreating(modelBuilder);
+    }
 }
