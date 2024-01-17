25d4a307af768f74c66f7a948333bf82cb577f3b
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-18 01:00:43 +0300

    ExpenseApplication/Api/Api.csproj

---
+++ ExpenseApplication/Api/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,48 @@
+using Newtonsoft.Json;
+
+namespace Api.Middlewares;
+
+public class GlobalExceptionHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public GlobalExceptionHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next ?? throw new ArgumentNullException(nameof(next));
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception, int statusCode = StatusCodes.Status500InternalServerError)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = statusCode;
+
+        return context.Response.WriteAsync(new ErrorDetails
+        {
+            StatusCode = statusCode,
+            Message = exception.Message
+        }.ToString());
+    }
+}
+
+public class ErrorDetails
+{
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+
+    public override string ToString()
+    {
+        return JsonConvert.SerializeObject(this);
+    }
+}
