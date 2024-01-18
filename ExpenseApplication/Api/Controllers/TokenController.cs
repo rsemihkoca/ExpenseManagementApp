4b7e85ba99d6b308564241a136d07fe7bba5948e
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-19 00:47:08 +0300

    M ExpenseApplication/Api/appsettings.json

--- ExpenseApplication/Api/Controllers/TokenController.cs
+++ ExpenseApplication/Api/Controllers/TokenController.cs
@@ -1,6 +1,28 @@
+using Application.Cqrs;
+using Infrastructure.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Api.Controllers;
 
-public class TokenController
+
+[Route("api/[controller]")]
+[ApiController]
+public class TokenController : ControllerBase
 {
+    private readonly IMediator mediator;
+
+    public TokenController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
     
+            
+    [HttpPost]
+    public async Task<TokenResponse> Post([FromBody] TokenRequest request)
+    {
+        var operation = new CreateTokenCommand(request);
+        var result = await mediator.Send(operation);
+        return result;
+    }
 }
