ed39ba5002a04f40c3a2329cf9fda3c98f3ec2b1
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-18 23:05:30 +0300

    ExpenseApplication/Infrastructure/Token/JwtConfig.cs

--- ExpenseApplication/Infrastructure/Token/JwtConfig.cs
+++ ExpenseApplication/Infrastructure/Token/JwtConfig.cs
@@ -2,5 +2,8 @@ namespace Infrastructure.Token;
 
 public class JwtConfig
 {
-    
+    public string Secret { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public int AccessTokenExpiration { get; set; }
 }
