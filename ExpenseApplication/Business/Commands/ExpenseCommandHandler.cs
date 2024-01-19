87622909f23da9afd8c63f08f1e71cc8f9a0f936
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-19 05:40:14 +0300

    ExpenseApplication/Business/Commands/ExpenseCommandHandler.cs

--- ExpenseApplication/Business/Commands/ExpenseCommandHandler.cs
+++ ExpenseApplication/Business/Commands/ExpenseCommandHandler.cs
@@ -1,6 +1,30 @@
+using Application.Cqrs;
+using Infrastructure.Data.DbContext;
+using Infrastructure.Dtos;
+using Infrastructure.Token;
+using MediatR;
+using Microsoft.Extensions.Options;
+
 namespace Application.Commands;
 
-public class ExpenseCommandHandler
+public class ExpenseCommandHandler :
+    IRequestHandler<CreateExpenseCommand, ExpenseResponse>
 {
-    
+    private readonly ExpenseDbContext dbContext;
+    private readonly JwtConfig jwtConfig;
+
+    public ExpenseCommandHandler(ExpenseDbContext dbContext, IOptionsMonitor<JwtConfig> jwtConfig)
+    {
+        this.dbContext = dbContext;
+        this.jwtConfig = jwtConfig.CurrentValue;
+    }
+
+
+    public async Task<ExpenseResponse> Handle(CreateExpenseCommand request, CancellationToken cancellationToken)
+    {
+        // category validation user validation amount validation 
+        // check float category id will be casted or what
+        // cast category name to id 
+        return await Task.FromResult(new ExpenseResponse());
+    }
 }
