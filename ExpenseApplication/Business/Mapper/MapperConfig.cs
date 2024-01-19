c6f4c62f9ae43c9642ab720ac0ad9205c3733750
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-20 00:03:24 +0300

    Http Exception, Create Token And Validations:ExpenseApplication/Business/Mapper/MapperConfig.cs

--- ExpenseApplication/Business/Mapper/MapperConfig.cs
+++ ExpenseApplication/Business/Mapper/MapperConfig.cs
@@ -1,5 +1,7 @@
 using AutoMapper;
 using Business.Entities;
+using Business.Enums;
+
 using Infrastructure.Dtos;
 
 namespace Application.Mapper;
@@ -8,15 +10,28 @@ public class MapperConfig : Profile
 {
     public MapperConfig()
     {
+    
+        CreateMap<InsertExpenseRequest, Expense>()
+            .ForMember(dest => dest.Status,
+                opt => opt.MapFrom(src => ExpenseRequestStatus.Pending))
+            .ForMember(dest => dest.CreationDate,
+                opt => opt.MapFrom(src => DateTime.UtcNow))
+            .ForMember(dest => dest.LastUpdateTime,
+                opt => opt.MapFrom(src => DateTime.UtcNow));
+        
+        
+
         CreateMap<Expense, ExpenseResponse>()
             .ForMember(dest => dest.PersonnelName,
                 opt => opt.MapFrom(src => src.User.FirstName + " " + src.User.LastName))
             .ForMember(dest => dest.CategoryName,
                 opt => opt.MapFrom(src => src.ExpenseCategory.CategoryName))
             .ForMember(dest => dest.ExpenseStatus,
-                opt => opt.MapFrom(src => src.Status.ToString()))
-            .ForMember(dest => dest.PaymentStatus,
-                opt => opt.MapFrom(src => src.PaymentInstruction.PaymentStatus.ToString()));
-
+                opt => opt.MapFrom(src => EnumUtils.GetDescription(src.Status)))
+            .ForMember(src => src.CreationDate,
+                opt => opt.MapFrom(src => src.CreationDate.ToString("dd/MM/yyyy HH:mm:ss")))
+            .ForMember(src => src.LastUpdateTime,
+                opt => opt.MapFrom(src => src.LastUpdateTime.ToString("dd/MM/yyyy HH:mm:ss")));
+        
     }
 }
