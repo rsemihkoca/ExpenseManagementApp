9626e87d6627a3b969f8b15ef62f5e4f0315784c
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-20 00:03:24 +0300

    Http Exception, Create Token And Validations:ExpenseApplication/Schemas/Dtos/Expense.cs

--- ExpenseApplication/Schemas/Dtos/Expense.cs
+++ ExpenseApplication/Schemas/Dtos/Expense.cs
@@ -4,19 +4,14 @@ namespace Infrastructure.Dtos;
 
 public class InsertExpenseRequest
 {
-    // public int ExpenseRequestId { get; set; }
-    // public int UserId { get; set; } from JWT
+    public int UserId { get; set; }
     public double Amount { get; set; }
     public int CategoryId { get; set; }
     public string PaymentMethod { get; set; }
     public string PaymentLocation { get; set; }
 
     public string Documents { get; set; }
-
-    // public ExpenseRequestStatus Status { get; set; } // (Pending
     public string Description { get; set; }
-    // public DateTime CreationDate { get; set; }
-    // public DateTime LastUpdateTime { get; set; }
 }
 
 
@@ -36,16 +31,15 @@ public class UpdateExpenseRequest //     public int ExpenseRequestId { get; set;
 public class ExpenseResponse
 {
     public int ExpenseRequestId { get; set; } // (Primary Key)
-    public int PersonnelName { get; set; } // (Foreign Key)
+    public string PersonnelName { get; set; } // (Foreign Key)
     public string CategoryName { get; set; } // (Foreign Key to ExpenseCategory)
-    public ExpenseRequestStatus ExpenseStatus { get; set; }
-    public string PaymentStatus { get; set; }
+    public string ExpenseStatus { get; set; }
 
     public double Amount { get; set; }
     public string PaymentMethod { get; set; }
     public string PaymentLocation { get; set; }
     public string Documents { get; set; }
     public string Description { get; set; }
-    public DateTime CreationDate { get; set; }
-    public DateTime LastUpdateTime { get; set; }
+    public string CreationDate { get; set; }
+    public string LastUpdateTime { get; set; }
 }
