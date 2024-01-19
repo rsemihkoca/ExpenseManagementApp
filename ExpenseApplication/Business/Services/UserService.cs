67b085fd036cd55aa9530b44104384785d482f99
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-20 00:03:24 +0300

    Http Exception, Create Token And Validations:ExpenseApplication/Business/Services/UserService.cs

--- ExpenseApplication/Business/Services/UserService.cs
+++ ExpenseApplication/Business/Services/UserService.cs
@@ -1,6 +1,43 @@
+using System.Security.Claims;
+using Infrastructure.Exceptions;
+using Microsoft.AspNetCore.Http;
+
 namespace Application.Services;
 
-public class UserService
+public class UserService(IHttpContextAccessor httpContextAccessor) : IUserService
 {
+    public string GetUserRole()
+    {
+        var role = httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Role)?.Value;
+        
+        if (role == null)
+        {
+            throw new HttpException("Role not found", 404);
+        }
+
+        return role;
+    }
+
+    public string GetUserEmail()
+    {
+        var email = httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Email)?.Value;
+        
+        if (email == null)
+        {
+            throw new HttpException("Email not found", 404);
+        }
+
+        return email;
+    }
     
+    public int GetUserId()
+    {
+        var userId = httpContextAccessor.HttpContext?.User.FindFirst("Id")?.Value;
+        
+        if (userId == null)
+        {
+            throw new HttpException("User not found", 404);
+        }
+        return int.Parse(userId);
+    }
 }
