85c2a6fcfa4021be7b88052c3f748d6d744c4ec0
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-19 06:29:31 +0300

    Convert Enum to String In Database:ExpenseApplication/Infrastructure/Enums/Enum.cs

--- ExpenseApplication/Infrastructure/Enums/Enum.cs
+++ ExpenseApplication/Infrastructure/Enums/Enum.cs
@@ -1,3 +1,5 @@
+using Business.Entities;
+
 namespace Business.Enums;
 
 public enum UserRole
@@ -5,6 +7,35 @@ public enum UserRole
     Admin,
     Personnel
 }
+//
+// public sealed class UserRole
+// {
+//     public const string Admin = "Admin";
+//     public const string Personnel = "Personnel";
+//     private readonly string _value;
+//     
+//     private UserRole(string value)
+//     {
+//         _value = value;
+//     }
+//     
+//     public static implicit operator string(UserRole userRole)
+//     {
+//         return userRole._value;
+//     }
+//     public static implicit operator UserRole(string Role)
+//     {
+//         switch (Role)
+//         {
+//             case Admin:
+//                 return new UserRole(Admin);
+//             case Personnel:
+//                 return new UserRole(Personnel);
+//             default:
+//                 throw new ArgumentException("Invalid Role");
+//         }
+//     }
+// }
 
 public enum ExpenseRequestStatus
 {
