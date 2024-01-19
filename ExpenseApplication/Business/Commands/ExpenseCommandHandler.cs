d004a8d30464b12c2d8a2c9447d9d20bfc3f31ad
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-20 00:03:24 +0300

    Http Exception, Create Token And Validations:ExpenseApplication/Business/Commands/ExpenseCommandHandler.cs

--- ExpenseApplication/Business/Commands/ExpenseCommandHandler.cs
+++ ExpenseApplication/Business/Commands/ExpenseCommandHandler.cs
@@ -1,9 +1,12 @@
 using Application.Cqrs;
+using Application.Services;
+using Application.Validators;
+using AutoMapper;
+using Business.Entities;
 using Infrastructure.Data.DbContext;
 using Infrastructure.Dtos;
-using Infrastructure.Token;
+using Infrastructure.Exceptions;
 using MediatR;
-using Microsoft.Extensions.Options;
 
 namespace Application.Commands;
 
@@ -11,35 +14,60 @@ public class ExpenseCommandHandler :
     IRequestHandler<CreateExpenseCommand, ExpenseResponse>
 {
     private readonly ExpenseDbContext dbContext;
-    private readonly JwtConfig jwtConfig;
 
-    public ExpenseCommandHandler(ExpenseDbContext dbContext, IOptionsMonitor<JwtConfig> jwtConfig)
+    // private readonly JwtConfig jwtConfig;
+    private readonly IUserService userService;
+    private readonly IMapper mapper;
+    private readonly IHandlerValidator validator;
+
+    public ExpenseCommandHandler(ExpenseDbContext dbContext, IMapper mapper, IHandlerValidator validator,
+        IUserService userService) // IOptionsMonitor<JwtConfig> jwtConfig,
     {
         this.dbContext = dbContext;
-        this.jwtConfig = jwtConfig.CurrentValue;
+        this.mapper = mapper;
+        this.validator = validator;
+        this.userService = userService;
+        // this.jwtConfig = jwtConfig.CurrentValue;
     }
 
 
     public async Task<ExpenseResponse> Handle(CreateExpenseCommand request, CancellationToken cancellationToken)
     {
-        
-        // category validation user validation amount validation 
-        // check float category id will be casted or what
-        // cast category name to id 
-        
-        /* personel sadece kendisi için expense oluşturabilir
-         * admin herkes için expense oluşturabilir
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         * 
-         */
-        return await Task.FromResult(new ExpenseResponse());
+        await validator.ValidateUserAsync(request.Model.UserId, cancellationToken);
+        await validator.ValidateCategoryAsync(request.Model.CategoryId, cancellationToken);
+
+        var role = userService.GetUserRole();
+
+        switch (role)
+        {
+            case "Admin":
+                break;
+            case "Personnel":
+                if (request.Model.UserId != userService.GetUserId())
+                {
+                    throw new HttpException("You can't create expense for other users", 403);
+                }
+                break;
+            default:
+                throw new HttpException("You can't create expense", 403);
+        }
+        // + // Get role from jwttoken
+        // + // check user is admin or not
+        // + // check user is exist or not
+        // + // check category is exist or not
+        // check category id is integer or not
+        // check amount is integer or not
+        // 
+        // personel sadece kendisi için expense oluşturabilir
+        // admin herkes için expense oluşturabilir
+        //
+
+        var entity = mapper.Map<InsertExpenseRequest, Expense>(request.Model);
+
+        var entityResult = await dbContext.AddAsync(entity, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        var mapped = mapper.Map<Expense, ExpenseResponse>(entityResult.Entity);
+        return mapped;
     }
 }
