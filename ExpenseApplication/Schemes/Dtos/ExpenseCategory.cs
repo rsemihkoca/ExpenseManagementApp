0da60bf938063566322ea90ce7190fd70cf30046
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-20 03:08:16 +0300

    ExpenseCategory Query:ExpenseApplication/Schemes/Dtos/ExpenseCategory.cs

--- ExpenseApplication/Schemes/Dtos/ExpenseCategory.cs
+++ ExpenseApplication/Schemes/Dtos/ExpenseCategory.cs
@@ -1,6 +1,17 @@
 namespace Infrastructure.Dtos;
 
-public class ExpenseCategory
+public class CreateExpenseCategoryRequest
 {
-    
+    public string CategoryName { get; set; }
+}
+
+public class UpdateExpenseCategoryRequest
+{
+    public string CategoryName { get; set; }
+}
+
+public class ExpenseCategoryResponse
+{
+    public int CategoryId { get; set; } // (Primary Key)
+    public string CategoryName { get; set; }
 }
