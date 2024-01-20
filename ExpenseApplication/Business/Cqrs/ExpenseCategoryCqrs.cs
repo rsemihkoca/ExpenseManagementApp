09e6a092739f09e132fc23711a4dc3c5e3a52633
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-20 03:08:16 +0300

    ExpenseCategory Query:ExpenseApplication/Business/Cqrs/ExpenseCategoryCqrs.cs

--- ExpenseApplication/Business/Cqrs/ExpenseCategoryCqrs.cs
+++ ExpenseApplication/Business/Cqrs/ExpenseCategoryCqrs.cs
@@ -1,9 +1,10 @@
+using Infrastructure.Dtos;
 using MediatR;
 
 namespace Application.Cqrs;
 
 public record CreateExpenseCategoryCommand(CreateExpenseCategoryRequest Model) : IRequest<ExpenseCategoryResponse>;
-public record UpdateExpenseCategoryCommand(int CategoryId,CreateExpenseCategoryRequest Model) : IRequest<ExpenseCategoryResponse>;
+public record UpdateExpenseCategoryCommand(int CategoryId, UpdateExpenseCategoryRequest Model) : IRequest<ExpenseCategoryResponse>;
 public record DeleteExpenseCategoryCommand(int CategoryId) : IRequest<ExpenseCategoryResponse>;
 
 public record GetAllExpenseCategoryQuery() : IRequest<List<ExpenseCategoryResponse>>;
