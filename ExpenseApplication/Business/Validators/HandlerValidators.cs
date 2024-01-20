44c43e021ef4a283e215444f502604cffae8d606
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-20 04:19:49 +0300

    ExpenseCategory Command Ok:ExpenseApplication/Business/Validators/HandlerValidators.cs

--- ExpenseApplication/Business/Validators/HandlerValidators.cs
+++ ExpenseApplication/Business/Validators/HandlerValidators.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using Business.Entities;
 using Infrastructure.Data.DbContext;
 using Infrastructure.Exceptions;
@@ -8,10 +9,13 @@ namespace Application.Validators;
 
 public interface IHandlerValidator
 {
-    Task<Boolean> ValidateUserAsync(int userId, CancellationToken cancellationToken);
-    Task<Boolean> ValidateCategoryAsync(int categoryId, CancellationToken cancellationToken);
-}
+    Task<User> ValidateUserIsExistAsync(int userId, CancellationToken cancellationToken);
+    Task<ExpenseCategory> ValidateCategoryIsExistAsync(int categoryId, CancellationToken cancellationToken);
 
+    Task<ExpenseCategory?> ValidateCategoryNameNotExistAsync(string modelCategoryName,
+        CancellationToken cancellationToken);
+    Task<bool> ValidateNoActiveEntityExistAsync(int id, Expression<Func<Expense, bool>> predicate, CancellationToken cancellationToken);
+}
 
 
 public class HandlerValidator : IHandlerValidator
@@ -23,7 +27,7 @@ public class HandlerValidator : IHandlerValidator
         this.dbContext = dbContext;
     }
 
-    public async Task<bool> ValidateUserAsync(int userId, CancellationToken cancellationToken)
+    public async Task<User> ValidateUserIsExistAsync(int userId, CancellationToken cancellationToken)
     {
         var user = await dbContext.Users.FirstOrDefaultAsync(x => x.UserId == userId, cancellationToken);
 
@@ -32,10 +36,10 @@ public class HandlerValidator : IHandlerValidator
             throw new HttpException("User not found", 404);
         }
 
-        return true;
+        return user;
     }
 
-    public async Task<bool> ValidateCategoryAsync(int categoryId, CancellationToken cancellationToken)
+    public async Task<ExpenseCategory> ValidateCategoryIsExistAsync(int categoryId, CancellationToken cancellationToken)
     {
         var category = await dbContext.ExpenseCategories.FirstOrDefaultAsync(x => x.CategoryId == categoryId, cancellationToken);
 
@@ -44,7 +48,35 @@ public class HandlerValidator : IHandlerValidator
             throw new HttpException("Category not found", 404);
         }
         
+        return category;
+    }
+    
+    public async Task<ExpenseCategory?> ValidateCategoryNameNotExistAsync(string categoryName, CancellationToken cancellationToken)
+    {
+        var category = await dbContext.ExpenseCategories.FirstOrDefaultAsync(x => x.CategoryName == categoryName.ToUpper(), cancellationToken);
+
+        if (category != null)
+        {
+            throw new HttpException("Category name already exist", 409);
+        }
+        
+        return category;
+    }
+    
+    public async Task<bool> ValidateNoActiveEntityExistAsync(int id, Expression<Func<Expense, bool>> predicate, CancellationToken cancellationToken)
+    {
+        var entity = await dbContext.Expenses.FirstOrDefaultAsync(predicate, cancellationToken);
+
+        if (entity != null)
+        {
+            throw new HttpException($"Active Expenses exist on this id: {id}", 405);
+        }
+
         return true;
     }
+    
+    
+    
+    
 }
 
