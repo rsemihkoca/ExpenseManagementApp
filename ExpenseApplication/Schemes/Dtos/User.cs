bc6a04a3ba963c0c41751af54626fa3f0090c388
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-20 08:57:12 +0300

    User and Category Handler, Controller:ExpenseApplication/Schemes/Dtos/User.cs

--- ExpenseApplication/Schemes/Dtos/User.cs
+++ ExpenseApplication/Schemes/Dtos/User.cs
@@ -1,6 +1,36 @@
 namespace Infrastructure.Dtos;
 
-public class User
+public class CreateUserRequest
 {
-    
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Iban { get; set; }
+    public string Role { get; set; }
+}
+
+public class UpdateUserRequest
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Iban { get; set; }
+    public string Role { get; set; }
+    public bool IsActive { get; set; }
+}
+
+public class UserResponse
+{
+    public string Username { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Iban { get; set; }
+    public string Role { get; set; }
+    public string IsActive { get; set; }
+    public string LastActivityDateTime { get; set; }
 }
