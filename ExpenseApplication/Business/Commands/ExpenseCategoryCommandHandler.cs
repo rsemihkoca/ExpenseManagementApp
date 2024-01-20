0ea129c8fafd0b88849311a681789578551c8967
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-20 03:08:16 +0300

    ExpenseCategory Query:ExpenseApplication/Business/Commands/ExpenseCategoryCommandHandler.cs

--- ExpenseApplication/Business/Commands/ExpenseCategoryCommandHandler.cs
+++ ExpenseApplication/Business/Commands/ExpenseCategoryCommandHandler.cs
@@ -1,6 +1,26 @@
+using Application.Cqrs;
+using Infrastructure.Dtos;
+using MediatR;
+
 namespace Application.Commands;
 
-public class ExpenseCategoryCommandHandler
+public class ExpenseCategoryCommandHandler :
+    IRequestHandler<CreateExpenseCategoryCommand, ExpenseCategoryResponse>,
+    IRequestHandler<UpdateExpenseCategoryCommand, ExpenseCategoryResponse>,
+    IRequestHandler<DeleteExpenseCategoryCommand, ExpenseCategoryResponse>
 {
-    
+    public async Task<ExpenseCategoryResponse> Handle(CreateExpenseCategoryCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ExpenseCategoryResponse> Handle(UpdateExpenseCategoryCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ExpenseCategoryResponse> Handle(DeleteExpenseCategoryCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
