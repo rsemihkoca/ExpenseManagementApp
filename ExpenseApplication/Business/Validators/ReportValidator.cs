987dbaf669d6aaf16e407bb13ef283f6661c368b
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-22 01:39:46 +0300

    Reports Implemented Succesfully:ExpenseApplication/Business/Validators/ReportValidator.cs

--- ExpenseApplication/Business/Validators/ReportValidator.cs
+++ ExpenseApplication/Business/Validators/ReportValidator.cs
@@ -14,3 +14,18 @@ public class ReportValidator : AbstractValidator<ReportFrequencyRequest>
         .WithMessage("Type must be daily, weekly or monthly");
     }
 }
+
+public class PersonnelSummaryValidator : AbstractValidator<PersonnelSummaryRequest>
+{
+    public PersonnelSummaryValidator()
+    {
+        RuleFor(x => x.Type)
+        .NotEmpty().WithMessage("Type is required")
+        .Must(x => x == "daily" || x == "weekly" || x == "monthly")
+        .WithMessage("Type must be daily, weekly or monthly");
+        
+        RuleFor(x => x.UserId)
+        .GreaterThan(0)
+        .WithMessage("UserId must be greater than 0");
+    }
+}
