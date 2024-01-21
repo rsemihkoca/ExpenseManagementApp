4ed64dbcdb68723b2d0c3ae834bbc928e2362395
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-21 05:29:59 +0300

    Payment instruction:ExpenseApplication/Business/Cqrs/PaymentInstructionCqrs.cs

--- ExpenseApplication/Business/Cqrs/PaymentInstructionCqrs.cs
+++ ExpenseApplication/Business/Cqrs/PaymentInstructionCqrs.cs
@@ -1,6 +1,11 @@
+using Infrastructure.Dtos;
+using MediatR;
+
 namespace Application.Cqrs;
 
-public class PaymentInstructionCqrs
-{
-    
-}
+public record CreatePaymentInstructionCommand(CreatePaymentInstructionRequest Model) : IRequest<PaymentInstructionResponse>;
+public record UpdatePaymentInstructionCommand(int PaymentInstructionId, UpdatePaymentInstructionRequest Model) : IRequest<PaymentInstructionResponse>;
+public record DeletePaymentInstructionCommand(int PaymentInstructionId) : IRequest<PaymentInstructionResponse>;
+
+public record GetAllPaymentInstructionQuery() : IRequest<List<PaymentInstructionResponse>>;
+public record GetPaymentInstructionByIdQuery(int PaymentInstructionId) : IRequest<PaymentInstructionResponse>;
