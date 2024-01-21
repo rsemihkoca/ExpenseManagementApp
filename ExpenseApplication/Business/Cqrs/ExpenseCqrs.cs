5e659e7def0e877264f0d07b140193192836fdc6
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-21 08:52:04 +0300

    ExpenseApplication/Business/Cqrs/ExpenseCqrs.cs

--- ExpenseApplication/Business/Cqrs/ExpenseCqrs.cs
+++ ExpenseApplication/Business/Cqrs/ExpenseCqrs.cs
@@ -4,12 +4,10 @@ using MediatR;
 namespace Application.Cqrs;
 
 
-// common endpoint for all entites
-
 public record CreateExpenseCommand(CreateExpenseRequest Model) : IRequest<ExpenseResponse>;
 
 public record UpdateExpenseCommand(int ExpenseRequestId, UpdateExpenseRequest Model) : IRequest<ExpenseResponse>;
-// admin kendini onaylayamaz
+
 public record DeleteExpenseCommand(int ExpenseRequestId) : IRequest<ExpenseResponse>;
 
 public record ApproveExpenseCommand(int ExpenseRequestId) : IRequest<ExpenseResponse>;
@@ -19,6 +17,5 @@ public record RejectExpenseCommand(int ExpenseRequestId) : IRequest<ExpenseRespo
 
 
 public record GetAllExpenseQuery() : IRequest<List<ExpenseResponse>>;
-
 public record GetExpenseByIdQuery(int ExpenseRequestId) : IRequest<ExpenseResponse>;
 public record GetExpenseByParameterQuery(GetExpenseByParameterRequest Model) : IRequest<List<ExpenseResponse>>;
