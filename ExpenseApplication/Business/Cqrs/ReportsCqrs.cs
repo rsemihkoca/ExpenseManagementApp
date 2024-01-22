5d84d90fcba89b06fc3291e4352c5cf262f7d978
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-22 11:36:13 +0300

    Final:ExpenseApplication/Business/Cqrs/ReportsCqrs.cs

--- ExpenseApplication/Business/Cqrs/ReportsCqrs.cs
+++ ExpenseApplication/Business/Cqrs/ReportsCqrs.cs
@@ -1,18 +1,11 @@
-using Business.Enums;
 using Infrastructure.Dtos;
 using MediatR;
 
-namespace Application.Cqrs;
+namespace Business.Cqrs;
 
-// all of them daily, weekly, monthly
-//  admin
 public record ApprovedPaymentFrequencyReport(ReportFrequencyRequest Model) : IRequest<ApprovedPaymentFrequencyReportResponse>;
 public record RejectedPaymentFrequencyReport(ReportFrequencyRequest Model) : IRequest<RejectedPaymentFrequencyReportResponse>;
 
-//  admin
-// group by user id
 public record PersonnelExpenseFrequencyReport(ReportFrequencyRequest Model) : IRequest<PersonnelExpenseFrequencyReportResponse>;
 
-// personnel and admin
-// personnel can only see their own
 public record PersonnelSummaryReport(PersonnelSummaryRequest Model) : IRequest<PersonnelSummaryReportResponse>;
