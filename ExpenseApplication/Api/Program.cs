bfba0af78fd6d24aaadb9df406f0d97b39e04870
Riza Semih Koca <rsemihkoca@outlook.com>
2024-01-22 11:36:13 +0300

    Final:ExpenseApplication/Api/Program.cs

--- ExpenseApplication/Api/Program.cs
+++ ExpenseApplication/Api/Program.cs
@@ -5,11 +5,8 @@ public class Program
     public static void Main(string[] args)
     {
         var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-        // Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(config).CreateLogger();
-        // Log.Information("App server is starting.");
         
         Host.CreateDefaultBuilder(args)
-            // .UseSerilog()
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
